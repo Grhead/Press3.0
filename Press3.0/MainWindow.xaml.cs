edc424424735acd0ff4a064a664d3e079781cdf7
Egor <orlovegor17@gmail.com>
2022-01-23 13:00:40 +0700

    Cleanup code

--- Press3.0/MainWindow.xaml.cs
+++ Press3.0/MainWindow.xaml.cs
@@ -2,48 +2,49 @@
 using System.Windows;
 using System.Windows.Threading;
 
-namespace Press3._0
+namespace Press3._0;
+
+public partial class MainWindow
 {
-    public partial class MainWindow
+    private readonly Cx _db;
+
+    public MainWindow()
     {
-        private readonly Cx _db;
-        public MainWindow()
+        InitializeComponent();
+        _db = new Cx();
+        var timer = new DispatcherTimer
         {
-            InitializeComponent();
-            _db = new Cx();
-            DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(1);
-            timer.Tick += TimerTick;
-            timer.Start();
-            void TimerTick(object sender, EventArgs e)
-            {
-                LblTime.Content = DateTime.Now.ToLongTimeString();
-            }
+            Interval = TimeSpan.FromSeconds(1)
+        };
+        timer.Tick += TimerTick;
+        timer.Start();
 
-        }
-
-        private void Handx_sd(object sender, RoutedEventArgs e)
+        void TimerTick(object sender, EventArgs e)
         {
-            Sobutie();
+            LblTime.Content = DateTime.Now.ToLongTimeString();
         }
+    }
 
-        static bool Sobutie()
-        {
-            bool handx = true;
-            return handx;
-        }
-        private void Send_Click(object sender, RoutedEventArgs e)
-        {
-            DateTime dateandTime = DateTime.Now;
-            int upPressure = Convert.ToInt32(Up.Text);
-            int downPressure = Convert.ToInt32(Down.Text);
-            string comment = (Commentx.Text);
-            bool hand = Sobutie();
-            Hlass sd = new Hlass(dateandTime, upPressure, downPressure, comment, hand);
-            _db.Main.Add(sd);
-            _db.SaveChanges();
-        }
+    private void Handx_sd(object sender, RoutedEventArgs e)
+    {
+        Sobutie();
+    }
 
-        
+    private static bool Sobutie()
+    {
+        var handx = true;
+        return handx;
+    }
+
+    private void Send_Click(object sender, RoutedEventArgs e)
+    {
+        var dateandTime = DateTime.Now;
+        var upPressure = Convert.ToInt32(Up.Text);
+        var downPressure = Convert.ToInt32(Down.Text);
+        var comment = Commentx.Text;
+        var hand = Sobutie();
+        var sd = new Hlass(dateandTime, upPressure, downPressure, comment, hand);
+        _db.Main.Add(sd);
+        _db.SaveChanges();
     }
 }
